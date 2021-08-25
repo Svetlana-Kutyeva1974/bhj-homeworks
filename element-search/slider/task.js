9063b97fdbc4aef44cfe7133abc5803511b10c4a
Svetlana-Kutyeva1974 <81332075+Svetlana-Kutyeva1974@users.noreply.github.com>
2021-08-26 02:22:53 +0300

    1 commit

---
+++ element-search/slider/task.js
@@ -0,0 +1,51 @@
+let activeArrowPrev = document.querySelector("div.slider__arrow_prev");
+let activeArrowNext = document.querySelector("div.slider__arrow_next");
+const getAllSlider = document.querySelectorAll("div.slider__item"); 
+console.log(getAllSlider);
+let arr = Array.from(getAllSlider);
+//let ActiveIndex = arr.findIndex()
+
+activeArrowPrev.onclick = function ()
+{
+	let activeSlider = document.querySelector("div.slider__item_active");
+	if (activeSlider!= null) {
+		activeSlider.classList.remove("slider__item_active");
+	}
+	if (activeSlider.previousElementSibling != null )
+		{activeSlider.previousElementSibling.classList.add("slider__item_active");}
+	else
+		{activeSlider.nextElementSibling.classList.add("slider__item_active");}
+
+
+}
+activeArrowNext.onclick = function ()
+{
+	let activeSlider = document.querySelector("div.slider__item_active");
+	if (activeSlider!= null) {
+		activeSlider.classList.remove("slider__item_active");
+	}
+	if (activeSlider.nextElementSibling != null )
+		{activeSlider.nextElementSibling.classList.add("slider__item_active");}
+	else
+		{activeSlider.previousElementSibling.classList.add("slider__item_active");}
+}
+
+// points
+
+const getAllDots= document.querySelectorAll("div.slider__dot"); 
+let arrDots = Array.from(getAllDots);
+for (let i = 0; i<length; i++)
+{
+	arrDots[i].onclick = function ()
+	{
+		activeMenu.classList.add("slider__dot_active");
+
+		let activeSlider = document.querySelector("div.slider__item_active");
+		console.log(activeSlider);
+		if (activeSlider!= null) {
+			activeSlider.classList.remove("slider__item_active");
+		}
+		arr[i].classList.add("slider__item_active");
+
+	}
+}
