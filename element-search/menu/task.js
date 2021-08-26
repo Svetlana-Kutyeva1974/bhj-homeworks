1a61c2516e26db16b2b1c91562e70f152728c8d2
Svetlana-Kutyeva1974 <81332075+Svetlana-Kutyeva1974@users.noreply.github.com>
2021-08-26 14:48:51 +0300

    2 commit

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,11 +1,12 @@
 const getMenuPoint = document.querySelectorAll("a.menu__link"); 
 let arr = Array.from(getMenuPoint);
 const length = arr.length;
+
 for (let i = 0; i<length; i++)
 {
 	arr[i].onclick = function ()
 	{
-		let activeMenu = document.querySelector("ul.menu_active");
+		  let activeMenu = document.querySelector("ul.menu_active");
 			if (activeMenu != null) {
 				activeMenu.classList.remove("menu_active");
 			}
