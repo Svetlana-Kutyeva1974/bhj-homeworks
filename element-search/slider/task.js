31651c0c7cd671c5182265157f84b5100fcf1179
Svetlana-Kutyeva1974 <81332075+Svetlana-Kutyeva1974@users.noreply.github.com>
2021-08-26 14:49:51 +0300

    2 commit

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,15 +1,11 @@
 let activeArrowPrev = document.querySelector("div.slider__arrow_prev");
 let activeArrowNext = document.querySelector("div.slider__arrow_next");
 const getAllSlider = document.querySelectorAll("div.slider__item"); 
-console.log(getAllSlider);
 let arr = Array.from(getAllSlider);
-
 const getAllDots= document.querySelectorAll("div.slider__dot"); 
 let arrDots = Array.from(getAllDots);
-console.log(arrDots);
-
+let length = arrDots.length;
 let ActiveIndex = arr.findIndex(() => document.querySelector("div.slider__item_active"));
-console.log(ActiveIndex);
 
 activeArrowPrev.onclick = function ()
 {
@@ -21,8 +17,6 @@ activeArrowPrev.onclick = function ()
 		{activeSlider.previousElementSibling.classList.add("slider__item_active");}
 	else
 		{activeSlider.parentElement.lastElementChild.classList.add("slider__item_active");}
-
-
 }
 activeArrowNext.onclick = function ()
 {
@@ -38,21 +32,16 @@ activeArrowNext.onclick = function ()
 
 // points
 
-//const getAllDots= document.querySelectorAll("div.slider__dot"); 
-//let arrDots = Array.from(getAllDots);
-
 for (let i = 0; i<length; i++)
 {
 	arrDots[i].onclick = function ()
 	{
 		arrDots[i].classList.add("slider__dot_active");
 		let activeSlider = document.querySelector("div.slider__item_active");
-		console.log(activeSlider);
 		if (activeSlider != null) {
 			activeSlider.classList.remove("slider__item_active");
 		}
 		arr[i].classList.add("slider__item_active");
-		console.log(arr[i]);
 
 	}
 }
