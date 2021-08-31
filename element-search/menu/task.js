63426c98256a11cceb2fdf33dd206fd813697dfd
Svetlana-Kutyeva1974 <81332075+Svetlana-Kutyeva1974@users.noreply.github.com>
2021-08-31 15:56:20 +0300

    3

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,21 +1,96 @@
-const getMenuPoint = document.querySelectorAll("a.menu__link"); 
-let arr = Array.from(getMenuPoint);
-const length = arr.length;
 
-for (let i = 0; i<length; i++)
+let arrLinks = Array.from(document.querySelectorAll("a.menu__link"));
+for (let linkClick of arrLinks)
 {
-	arr[i].onclick = function ()
+	linkClick.onclick = function ()
 	{
-		  let activeMenu = document.querySelector("ul.menu_active");
-			if (activeMenu != null) {
+		 let activeMenu = document.querySelector("ul.menu_active");
+				if (linkClick.nextElementSibling.classList.contains("menu_active")) {
+				linkClick.nextElementSibling.classList.remove("menu_active");
+				return;
+			}
+			 
+			if (linkClick.nextElementSibling != null && !(linkClick.nextElementSibling.classList.contains("menu_active"))) {
+				if (activeMenu != null) {
 				activeMenu.classList.remove("menu_active");
 			}
-			if (arr[i].nextElementSibling != null) {
-	    	arr[i].nextElementSibling.classList.add("menu_active");
+				linkClick.nextElementSibling.classList.add("menu_active");
 	    	return false;
-	    }
-	    if (arr[i].parentElement.parentElement.className === "menu menu_sub") {
-	    	return false;
-	    }
-	  };
+			}
+	  }
 }
+
+
+
+
+
+
+
+//--------------------------------------------------------------------------
+
+// два меню
+/*function variantMenu() {
+const linkClick2 = this.closest(".menu");
+		 let activeMenu = document.querySelector("ul.menu_active");
+				if (linkClick2.nextElementSibling.classList.contains("menu_active")) {
+				linkClick2.nextElementSibling.classList.remove("menu_active");
+				return;
+			}
+			
+			else if (linkClick2.nextElementSibling != null) {
+				if (activeMenu != null) {
+				activeMenu.classList.remove("menu_active");
+			}
+				linkClick2.nextElementSibling.classList.add("menu_active");
+	    	return false;
+			}
+}
+
+let arrLinks = Array.from(document.querySelectorAll("a.menu__link"));
+for (let linkClick of arrLinks)
+{
+	linkClick.onclick = variantMenu;
+}
+*/
+
+
+
+// два меню
+/*function variantMenu() {
+const linkClick2 = this.closest(".menu");
+		 let activeMenu = document.querySelector("ul.menu_active");
+				if (linkClick2.nextElementSibling.classList.contains("menu_active")) {
+				linkClick2.nextElementSibling.classList.remove("menu_active");
+				return;
+			}
+			
+			else if (linkClick2.nextElementSibling != null) {
+				if (activeMenu != null) {
+				activeMenu.classList.remove("menu_active");
+			}
+				linkClick2.nextElementSibling.classList.add("menu_active");
+	    	return false;
+			}
+}
+
+let arrLinks = Array.from(document.querySelectorAll("a.menu__link"));
+for (let linkClick of arrLinks)
+{
+	linkClick.onclick = variantMenu;
+}
+*/
+
+
+
+
+
+
+
+
+
+/*
+let index = arrLinks.findIndex((item) => {
+			 	console.log("иntv" + item);
+			 	item.nextElementSibling.classList.contains("menu_active") == true});
+			console.log("индекс " + index);
+*/
