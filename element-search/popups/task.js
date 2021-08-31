0db7d27c0ce1bd136123453d30ad5ec128bdcacf
Svetlana-Kutyeva1974 <81332075+Svetlana-Kutyeva1974@users.noreply.github.com>
2021-08-31 15:55:12 +0300

    3

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,4 +1,36 @@
 const element = document.getElementById("modal_main");
+const arrClose = Array.from(document.querySelectorAll("div.modal__close_times"));
+element.classList.add("modal_active");
+
+function closeModal() {
+const modalParent = this.closest(".modal");
+modalParent.classList.remove("modal_active");
+modalParent.classList.add("modal__close");
+}
+
+for (let i of arrClose)
+{
+  i.onclick = closeModal;
+}
+
+const elementSuccess = document.getElementById("modal_success");
+const elementForSuccess = document.querySelector("a.show-success"); 
+elementForSuccess.onclick = function() {
+  closeModal.call(element);
+  elementSuccess.classList.add("modal_active");
+};
+
+
+
+
+
+
+
+
+
+
+//---------------------------------------------------------------------
+/*const element = document.getElementById("modal_main");
 const elementTimes = document.querySelectorAll("div.modal__close_times");
 let arr = Array.from(elementTimes);
 const length = arr.length;
@@ -19,3 +51,4 @@ elementForSuccess.onclick = function() {
   element.classList.add("modal__close");
   elementSuccess.classList.add("modal_active");
 };
+*/
