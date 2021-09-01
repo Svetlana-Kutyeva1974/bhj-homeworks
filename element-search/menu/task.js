a90b63f7c87b82162c8895751da5d50f12d19508
Svetlana-Kutyeva1974 <81332075+Svetlana-Kutyeva1974@users.noreply.github.com>
2021-09-01 03:57:04 +0300

    3

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,96 +1,25 @@
-
-let arrLinks = Array.from(document.querySelectorAll("a.menu__link"));
-for (let linkClick of arrLinks)
+let arrMenu = Array.from(document.getElementsByClassName("menu_main"));
+for (let menuClick of arrMenu)
 {
-	linkClick.onclick = function ()
+	let arrLinks = Array.from(menuClick.querySelectorAll("a.menu__link"));
+	for (let linkClick of arrLinks)
 	{
-		 let activeMenu = document.querySelector("ul.menu_active");
-				if (linkClick.nextElementSibling.classList.contains("menu_active")) {
-				linkClick.nextElementSibling.classList.remove("menu_active");
-				return;
-			}
-			 
+		linkClick.onclick = function ()
+		{
+			let activeMenu = menuClick.querySelector("ul.menu_active");
+			let activeMenus = Array.from(document.querySelectorAll("ul.menu_active"));
+			console.log(activeMenus.length);
+			if (linkClick.nextElementSibling.classList.contains("menu_active")) {
+				  linkClick.nextElementSibling.classList.remove("menu_active");
+				  return;
+			} 
 			if (linkClick.nextElementSibling != null && !(linkClick.nextElementSibling.classList.contains("menu_active"))) {
 				if (activeMenu != null) {
-				activeMenu.classList.remove("menu_active");
-			}
+					activeMenu.classList.remove("menu_active");
+				}
 				linkClick.nextElementSibling.classList.add("menu_active");
-	    	return false;
+				return false;
 			}
-	  }
+		}
+	}
 }
-
-
-
-
-
-
-
-//--------------------------------------------------------------------------
-
-// два меню
-/*function variantMenu() {
-const linkClick2 = this.closest(".menu");
-		 let activeMenu = document.querySelector("ul.menu_active");
-				if (linkClick2.nextElementSibling.classList.contains("menu_active")) {
-				linkClick2.nextElementSibling.classList.remove("menu_active");
-				return;
-			}
-			
-			else if (linkClick2.nextElementSibling != null) {
-				if (activeMenu != null) {
-				activeMenu.classList.remove("menu_active");
-			}
-				linkClick2.nextElementSibling.classList.add("menu_active");
-	    	return false;
-			}
-}
-
-let arrLinks = Array.from(document.querySelectorAll("a.menu__link"));
-for (let linkClick of arrLinks)
-{
-	linkClick.onclick = variantMenu;
-}
-*/
-
-
-
-// два меню
-/*function variantMenu() {
-const linkClick2 = this.closest(".menu");
-		 let activeMenu = document.querySelector("ul.menu_active");
-				if (linkClick2.nextElementSibling.classList.contains("menu_active")) {
-				linkClick2.nextElementSibling.classList.remove("menu_active");
-				return;
-			}
-			
-			else if (linkClick2.nextElementSibling != null) {
-				if (activeMenu != null) {
-				activeMenu.classList.remove("menu_active");
-			}
-				linkClick2.nextElementSibling.classList.add("menu_active");
-	    	return false;
-			}
-}
-
-let arrLinks = Array.from(document.querySelectorAll("a.menu__link"));
-for (let linkClick of arrLinks)
-{
-	linkClick.onclick = variantMenu;
-}
-*/
-
-
-
-
-
-
-
-
-
-/*
-let index = arrLinks.findIndex((item) => {
-			 	console.log("иntv" + item);
-			 	item.nextElementSibling.classList.contains("menu_active") == true});
-			console.log("индекс " + index);
-*/
