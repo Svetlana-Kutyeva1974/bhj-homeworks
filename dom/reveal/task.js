185cc921d556f5e1549c486a1d6607498c8bde85
Svetlana-Kutyeva1974 <81332075+Svetlana-Kutyeva1974@users.noreply.github.com>
2021-09-02 23:08:05 +0300

    1

---
+++ dom/reveal/task.js
@@ -0,0 +1,18 @@
+const arrReveal = Array.from(document.querySelectorAll("div.reveal"));
+
+let isInViewport = function(element) {
+  const viewportHeight = window.innerHeight;
+  const elementTop = element.getBoundingClientRect().top;
+  const elementBottom = element.getBoundingClientRect().bottom;
+  //return (elementTop > 0 && elementTop < viewportHeight) ? true : false;
+  return (elementTop > 0 && elementTop < viewportHeight) || (elementBottom.bottom < viewportHeight && elementBottom.bottom > 0) ? true : false;
+
+};
+
+const onScrollToggle = function() {
+  arrReveal.forEach((itemReveal) => {
+    console.log(isInViewport(itemReveal));
+    (isInViewport(itemReveal)) ? itemReveal.classList.add('reveal_active') : itemReveal.classList.remove('reveal_active');
+  });
+}
+window.addEventListener('scroll', onScrollToggle);
