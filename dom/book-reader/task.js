a94fda29a51cf40ec94e1ff2712e60d635488525
Svetlana-Kutyeva1974 <81332075+Svetlana-Kutyeva1974@users.noreply.github.com>
2021-09-04 21:55:30 +0300

    Update task.js

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -30,6 +30,33 @@ for (let link of arrLinksOfSize)
     //return false;
     //link.style.color = arrRotator[i].dataset.color;
   }
+
+function toggleBookColor() {
+      let  divBook= document.querySelector(div.book__content);
+    //let divBook div = document.getElementsById("book");
+    if (isActive() == 0) {
+      divBook.classList.add("book_fs-big");}
+    else if(isActive() == 2) {
+      divBook.classList.add("book_fs-small");
+     }  //иначе ничего не меняем
+    //event.preventDefault();
+    //return false;
+    //link.style.color = arrRotator[i].dataset.color;
+  }
+
+function toggleBookBackground() {
+      let  divBook= document.querySelector(div.book__content);
+    //let divBook div = document.getElementsById("book");
+    if (isActive() == 0) {
+      divBook.classList.add("book_fs-big");}
+    else if(isActive() == 2) {
+      divBook.classList.add("book_fs-small");
+     }  //иначе ничего не меняем
+    //event.preventDefault();
+    //return false;
+    //link.style.color = arrRotator[i].dataset.color;
+  }
+
   function onClick() {
     toggleSize();
     //toggleBookSize();
