60e44e0c4376f05f35c91d1c6a5758c9de88efcc
Svetlana-Kutyeva1974 <81332075+Svetlana-Kutyeva1974@users.noreply.github.com>
2021-09-04 13:38:57 +0300

    1

---
+++ dom/book-reader/task.js
@@ -0,0 +1,35 @@
+let arrLinksOfSize = Array.from(document.getElementsByClassName("font-size"));
+
+for (let link of arrLinksOfSize)
+{
+  //let arrRotator = Array.from(linkClick.querySelectorAll("span.rotator__case"));
+
+  function isActive () {
+    return (arrLinksOfSize.findIndex((item) => (item.classList.contains("font-size_active"))));
+  }
+
+  function toggleSize() {
+    arrLinksOfSize[isActive()].classList.remove("font-size_active");
+    link.classList.add("font-size_active");
+    console.log(link);
+    //event.preventDefault();
+    return false;
+    //link.style.color = arrRotator[i].dataset.color;
+  }
+
+  function toggleBookSize() {
+    let divBook = document.getElementsById("book");
+    //divBook.classList.add("book_fs-big");
+    //divBook.classList.add("book_fs-small");
+
+    //event.preventDefault();
+    return false;
+    //link.style.color = arrRotator[i].dataset.color;
+  }
+  function onClick() {
+    toggleSize();
+    //toggleBookSize();
+  }
+
+  link.addEventListener('click', onClick);
+}
