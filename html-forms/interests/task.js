7b0614ff1b1a08d8124c0d81417a4fbda1a052db
Svetlana-Kutyeva1974 <81332075+Svetlana-Kutyeva1974@users.noreply.github.com>
2021-09-07 17:20:08 +0300

    1

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,34 +1,117 @@
 const arrayInput = Array.from(document.querySelectorAll("input"));
 console.log(arrayInput);
 
+function countCheked(arr, check) {
+  let count = 0;
+  arr.forEach(child => {
+   if (child.checked === check.checked) {
+     count ++ ;
+   }
+ });
+  return count; 
+}
 
 addEventListener('change', e => {
   let check = e.target;
   let checkParent = check.closest("ul");
+  
+    if (checkParent.classList.contains("interests_active")) {
+
+      let array = checkParent.querySelectorAll("input");
+      let parentNodeInput = (checkParent.parentNode).querySelector("input");
+      let count = countCheked(array, check);
+
+      if (count !== array.length) {
+        (parentNodeInput.checked === check.checked) 
+        ? parentNodeInput.indeterminate = check.checked 
+        : parentNodeInput.indeterminate = true;
+      }
+      else {
+       parentNodeInput.indeterminate = false;
+       parentNodeInput.checked = check.checked;
+    	}
+   } 
+    else {
+      let arr = check.closest("label").nextElementSibling.querySelectorAll("input");
+      arr.forEach(child => child.checked = check.checked);
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//-----------------------мусор-------------------------------------
+/*
+
+
+const arrayInput = Array.from(document.querySelectorAll("input"));
+console.log(arrayInput);
+
+function countCheked(arr, check) {
+  let count = 0;
+  arr.forEach(child => {
+   if (child.checked === check.checked) {
+     count ++ ;
+   }
+   console.log("выбрано " + count);
+ });
+  return count; 
+}
+
+addEventListener('change', e => {
+  let check = e.target;
+  let checkParent = check.closest("ul");
+  //let arr = check.closest("label").nextElementSibling.querySelectorAll("input");
+
   //let arr = checkParent.firstElementChild.children[1].querySelectorAll("input");
   console.log(check);
   
     if (checkParent.classList.contains("interests_active")) {
-    //if (checkParent !== default) {
-  	(checkParent.parentNode).querySelector("input").indeterminate = check.checked;
-  	//check.checked = true;
+    // проверяем все ли подузлы выбраны
+      let array = checkParent.querySelectorAll("input");
+      let parentNodeInput = checkParent.parentNode.querySelector("input");
+      let count = countCheked(array, check);
 
-  	//check.children.checked = true;
-  	//check.children[0].checked = true;
-  } else {
-  	/*checkParent..children[0].checked = check.checked;
-    checkParent.children[1].checked = check.checked;*/
-    let arr = check.closest("label").nextElementSibling.querySelectorAll("input");
-    /*if (checkParent.firstElementChild.firstElementChild.children[0] == check) {
-    let arr = checkParent.firstElementChild.children[1].querySelectorAll("input");
-   }
-    else {
-      let arr = checkParent.lastElementChild.children[1].querySelectorAll("input");
-    }*/
-    arr.forEach(child => child.checked = check.checked);
-
-    //children.forEach(child => child.checked = check.checked);
-  }
+      if (count !== array.length) {
+      ((checkParent.parentNode).querySelector("input").checked === check.checked) ?
+      (checkParent.parentNode).querySelector("input").indeterminate = check.checked :
+      (checkParent.parentNode).querySelector("input").indeterminate = true;
+      }
+      else {
+       (checkParent.parentNode).querySelector("input").indeterminate = false;
+       (checkParent.parentNode).querySelector("input").checked = check.checked;
+        //
+      }
+   } 
+    else {// узел выбран
+      let arr = check.closest("label").nextElementSibling.querySelectorAll("input");
+      arr.forEach(child => child.checked = check.checked);
+    }
 });
 
-
+*/
