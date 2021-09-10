a693e8c0a6f21b3fa1668bdacfef8c5b188cc461
Svetlana-Kutyeva1974 <81332075+Svetlana-Kutyeva1974@users.noreply.github.com>
2021-09-10 21:10:52 +0300

    1

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -52,35 +52,52 @@ function onClickDec() {
   }
 
 function onClickAdd() {
+	console.log(this);
 	let buscet = document.querySelector('div.cart__products');
+	
+	let buscetAll = Array.from(buscet.querySelectorAll('div.cart__product'));
+//return (arr.findIndex((item) => (item.classList.contains("slider__item_active"))));
+	let l = buscetAll.findIndex((item) => (item.dataset.id === this.closest('.product').dataset.id));
+
 	let product = document.createElement('div');
 	let img = document.createElement('img');
 	let productCount = document.createElement('div');
-    
+     
+
+	if (buscetAll.length === 0 || l === -1 )
+
+    {
     product.classList.add("cart__product");
-    product.dataset.id = "1";// изменить
+   // let i = this.closest('.product');
+   // product.dataset.id = i.getAttribute('data-id');
+    product.dataset.id = this.closest('.product').dataset.id;
     //document.body.insertBefore(product,document.body.children[2]);
-    buscet.insertAdjacentElement('afterBegin', product);
+    //buscet.insertAdjacentElement('beforeEnd', product);
+     buscet.insertAdjacentElement('afterBegin', product);
+
 
     img.classList.add("cart__product-image");
-    img.src = "https://static-eu.insales.ru/images/products/1/7875/257179331/4515850.jpg";//изменить
+    img.src = this.closest('div.product').children[1].getAttribute('src');
+    //img.src = "https://static-eu.insales.ru/images/products/1/7875/257179331/4515850.jpg";//изменить
     //document.body.insertBefore(product,document.body.children[2]);
-    buscet.insertAdjacentElement('beforeEnd', img);
+
+    product.insertAdjacentElement('afterBegin', img);
 
     productCount.classList.add("cart__product-count");
-    productCount.innerText = "20";//изменить
+    //let i = this.closest('.product__quantity-controls');
+    let i = this.previousElementSibling.children[1];
+   // let ii = i.querySelector('div.product__quantity-value');
+    productCount.innerText = i.innerText;//"20";//изменить  
+
     //document.body.insertBefore(product,document.body.children[2]);
-    buscet.insertAdjacentElement('beforeEnd', productCount);
 
-   // buscet.insertBefore(tool,document.body.children[2]);
-    //this.closest('div.prodact').dataset.id;
-    //product.setAttribute('id',`${this.closest('div.prodact').dataset.id}`)
-   
-    //this.input.selectedIndex = item.index;
-    //this.valueElement.textContent = item.text;
-    // this.nextElementSibiling.innerHTML += 1;
-  }
+    product.insertAdjacentElement('beforeEnd', productCount);
+}
 
+else {
+	 buscetAll[l].children[1].innerText += this.previousElementSibling.children[1].innerText;
+}
+}
 
  document.querySelectorAll('div.product__quantity-control_dec' ).forEach((point) => {
 	  point.addEventListener('click', onClickDec);
