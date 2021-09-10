da0d02752667fbd9b0eeadbbb0b7d2afb24c9382
Svetlana-Kutyeva1974 <81332075+Svetlana-Kutyeva1974@users.noreply.github.com>
2021-09-10 16:01:42 +0300

    1

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -3,11 +3,11 @@ let tasks = document.querySelector("div.tasks__list");
 const but = document.querySelector("button.tasks__add");
 let nodes = tasks.children;
 //let nodes = nodesAll.querySelector("task__remove");
-let child = nodes.length-1;
-
+//let child = nodes.length-1;
+//let child = 0;
 
 let item = "";
-tasks.innerHTML = "";
+//tasks.innerHTML = "";
 
   function addTask (item) {
   tasks.innerHTML += `
@@ -18,30 +18,39 @@ tasks.innerHTML = "";
       <a href="#" class="task__remove">&times;</a>
     </div>
 `;
-  child += 1;
+  //child += 1;
 }
 
-function ClickNode() {
+function ClickNode () {
   event.preventDefault();
   //nodes.removeChild(`${nodes[child]}`);
-  nodes[child].remove();
+  console.log(event.target);
+  this.querySelector("a.task__remove").parentNode.remove();
+  //child -= 1;
 }
 
 const onKey = (e) => {
     console.log(e.key, e.code);
-    if (e.code === "Enter") {
+
+    if (e.code === "Enter" || e.code === "NumpadEnter") {
       event.preventDefault();
       addTask(item);
+       console.log((nodes.length-1));
       item = "";
       document.querySelector(".tasks__input").value = "";// очистка поля ввода
      // tasks.lastElementChild.scrollIntoView(false);//в конец окна
      //(nodes[child].querySelector("a.task__remove")).addEventListener('click', ClickNode);
-     (nodes[child].lastElementChild).addEventListener('click', ClickNode);
+
+     //(nodes[nodes.length-1].lastElementChild).addEventListener('click', ClickNode); 
+     //document.querySelector("a.task__remove").addEventListener('click', ClickNode);
+     // document.querySelector("div.task").addEventListener('click', ClickNode);
+      document.querySelector("div.tasks__list").addEventListener('click', ClickNode);
+      //child += 1;
+      //event.preventDefault();
     }
     else
     {
       item += `${e.key}`;
-      console.log(item);
     }
 }
 
@@ -49,22 +58,5 @@ function onClick(event) {
     document.addEventListener('keydown', onKey);
 }
 
-document.querySelector("div.card").addEventListener('click', onClick);
-
-
-
-
-
-
-/*for (let node of nodes) {
-  console.log(node.textContent);
-  console.log(node.querySelector("a.task__remove"));
-  node.querySelector("a.task__remove").addEventListener('click', function ClickNode() {
-
-   // node.querySelector("a.task__remove").closest("div.task").removeChild(node);
-    nodes.removeChild(node);
-    event.preventDefault();
-    return; 
-  });
-}
-*/
+//document.querySelector("div.card").addEventListener('click', onClick);
+document.querySelector("input.tasks__input").addEventListener('input', onClick);
