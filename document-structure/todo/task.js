d4a7c0594cf1d69dd3e1b013a7442ee5e84ac727
Svetlana-Kutyeva1974 <81332075+Svetlana-Kutyeva1974@users.noreply.github.com>
2021-09-10 16:25:00 +0300

    1

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,15 +1,7 @@
-
 let tasks = document.querySelector("div.tasks__list");
-const but = document.querySelector("button.tasks__add");
-let nodes = tasks.children;
-//let nodes = nodesAll.querySelector("task__remove");
-//let child = nodes.length-1;
-//let child = 0;
-
 let item = "";
-//tasks.innerHTML = "";
 
-  function addTask (item) {
+function addTaskHtml (item) {
   tasks.innerHTML += `
     <div class="task">
       <div class="task__title">
@@ -18,45 +10,29 @@ let item = "";
       <a href="#" class="task__remove">&times;</a>
     </div>
 `;
-  //child += 1;
 }
 
-function ClickNode () {
+function clickNodeRemove () {
   event.preventDefault();
-  //nodes.removeChild(`${nodes[child]}`);
-  console.log(event.target);
   this.querySelector("a.task__remove").parentNode.remove();
-  //child -= 1;
+}
+
+function AddElement(){
+  event.preventDefault();
+  addTaskHtml(item);
+  item = "";
+  document.querySelector(".tasks__input").value = "";// очистка поля ввода
+  document.querySelector("div.tasks__list").addEventListener('click', clickNodeRemove);
 }
 
 const onKey = (e) => {
-    console.log(e.key, e.code);
-
-    if (e.code === "Enter" || e.code === "NumpadEnter") {
-      event.preventDefault();
-      addTask(item);
-       console.log((nodes.length-1));
-      item = "";
-      document.querySelector(".tasks__input").value = "";// очистка поля ввода
-     // tasks.lastElementChild.scrollIntoView(false);//в конец окна
-     //(nodes[child].querySelector("a.task__remove")).addEventListener('click', ClickNode);
-
-     //(nodes[nodes.length-1].lastElementChild).addEventListener('click', ClickNode); 
-     //document.querySelector("a.task__remove").addEventListener('click', ClickNode);
-     // document.querySelector("div.task").addEventListener('click', ClickNode);
-      document.querySelector("div.tasks__list").addEventListener('click', ClickNode);
-      //child += 1;
-      //event.preventDefault();
-    }
-    else
-    {
-      item += `${e.key}`;
-    }
+  (e.code === "Enter" || e.code === "NumpadEnter") ? AddElement() : item += `${e.key}` ;
 }
 
 function onClick(event) {
     document.addEventListener('keydown', onKey);
 }
 
-//document.querySelector("div.card").addEventListener('click', onClick);
 document.querySelector("input.tasks__input").addEventListener('input', onClick);
+document.querySelector("button.tasks__add").addEventListener('click', AddElement);
+
