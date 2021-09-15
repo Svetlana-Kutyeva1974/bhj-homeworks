7870d041a42fd32564e8976a6b6c7e6b452c5b18
Svetlana-Kutyeva1974 <81332075+Svetlana-Kutyeva1974@users.noreply.github.com>
2021-09-15 21:42:22 +0300

    1

---
+++ client-state/popup/task.js
@@ -0,0 +1,72 @@
+const element = document.getElementById("subscribe-modal");
+const iClose = document.querySelector("div.modal__close_times");
+
+function saveCookie() {
+ document.cookie = 'close=true';
+}
+
+function getCookieFull() {
+//return JSON.parse(localStorage.getItem('obj'));
+document.cookie = 'user=' + encodeURIComponent('true');
+return document.cookie;
+}
+
+function closeModal() {
+const modalParent = this.closest(".modal");
+modalParent.classList.remove("modal_active");
+modalParent.classList.add("modal__close");
+//document.cookie = 'close=true';
+// console.log(document.cookie);
+}
+
+
+const getCookie = () => {
+//const value = document.cookie;
+const value = "; " + document.cookie;
+let parts = value.split("; " + modal__close + "=");
+if (parts.length === 2) {
+return parts
+.pop()
+.split(";")
+.shift();
+}
+/*let arrParts = document.cookie.split("=");
+return arrParts[1];*/
+}
+
+window.addEventListener("load", (event) => {
+	//getCookieFull();
+	console.log(document.cookie);
+	if (document.cookie !== "" && document.cookie !== null) {// работаем с куки
+		//if (document.cookie.split("=")[1] === "" || document.cookie.split("=")[1] === 'false') {
+			/*element.classList.add("modal_active");
+			iClose.onclick = closeModal;
+			event.preventDefault();
+	        saveCookie();*/
+		//}
+		console.log(document.cookie.split("=")[1]);
+	}
+		//document.querySelector('img.loader_active').classList.remove("loader_active");
+		//viewTable(obj);
+  else {
+   // обычное выполнение и запись кук
+     element.classList.add("modal_active");
+     iClose.onclick = closeModal;
+     
+     //document.cookie = 'close=true';
+     //console.log(document.cookie);
+     
+
+     //saveCookie();
+     //event.preventDefault();
+
+     window.addEventListener("beforeunload", (event) => {
+		   //document.cookie = 'close=true; path=file:///E:/0-Web-учеба';
+		  document.cookie = 'close=true';
+		  event.preventDefault();
+	    //saveCookie();
+	 });
+  }
+});
+
+
